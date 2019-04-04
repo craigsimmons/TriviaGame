2a1313309e27ab6e6b3650e1955ed56745028ef4
Craig Simmons <craig.a.simmons@gmail.com>
2019-04-03 17:16:04 -0700

    pretty much final version. may change colors

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-//Quiz data - how the fuck do I step through this? for loop not cutting it in way I've written the program flow.
+  
+// Quiz data - 10 questions.
 var quizMaterialArr = [
     {
       id: "1",
@@ -114,84 +115,128 @@ var quizMaterialArr = [
   ];
 
 /* Variables */
-var secondsLeftInt = 21;
+// Timer vars
+var secondsLeftInt = 15;
 var intervalId;
+// win loss vars
 var correctCountInt = 0;
 var incorrectCountInt = 0;
+// holds quiz question for display
 var quizQuestionStr = "";
+// holds quiz answers for display
 var answerAStr = "";
 var answerBStr = "";
 var answerCStr = "";
 var answerDStr = "";
+// holds the "correct" answer for evaluation
 var correctAnswerStr = "";
+// holds question Id - used to build HTML
 var positionId = "";
+// holds quizMaterialArray index for stepping through questions/answers
+// using this to track question "state" instead of a for loop
 var quizIndexInt = 0;
 
 /* Event Handlers */
-
+// Start button event handler
 $("#start").click(function() {
+  // reset wins and losses
     correctCountInt = 0;
     incorrectCountInt = 0;
     $("#gametimer").text(secondsLeftInt);
     $("#goodanswer").text(correctCountInt);
     $("#badanswer").text(incorrectCountInt);
+// disable and enable buttons - makes Start disabled (after it's pressed)
+    $("#start").attr("disabled", true);
+    $("#end").attr("disabled", false);
+    $("#submitbtn").attr("disabled", false);
+// build quiz and kick off timer
     buildQuiz();
     startCountdown();
-    // need to add code to disable start button
-    // fix timer issues 
 });
 
+//event handler for Reset (end) button
 $("#end").click(function() {
-    $("#gametimer").text("Game Over!");
+// message to player (in red) upon "ending/resetting game"
+    $("#gametimer").text("Game Over");
+  // $("#gametimer").css({"color":"red"});
+// update wins and losses
     $("#goodanswer").text(correctCountInt);
-  $("#badanswer").text(incorrectCountInt);
+    $("#badanswer").text(incorrectCountInt);
+    // disable and enable buttons - makes End and Submit buttons disabled
+    $("#start").attr("disabled", false);
+    $("#end").attr("disabled", true);
+    $("#submitbtn").attr("disabled", true);
+// end countdown, reset game and key variables
     endCountdown();
     resetGame();
-    // need to add code to enable start button
+    secondsLeftInt = 15;
 });
 
+//event handler for Submit button
+// end/restart timer and evaluate if answer is correct
 $("#submitbtn").click(function() {
-  endCountdown();
+    endCountdown();
+  startCountdown()
   $("#gametimer").text(secondsLeftInt);
   evalSubmission();
 });
 
 /* functions and core logic */
 
+// initial state of buttons - only start is enabled initially.
+// Reset/end & Submit becomes enabled once game starts. Toggle false or True argument
+$("#start").attr("disabled", false);
+$("#submitbtn").attr("disabled", true);
+$("#end").attr("disabled", true);
+
+// starts timer countdown. set to 11 (because 10 would start at 9)
 function startCountdown() {
-    secondsLeftInt = 21;
+    secondsLeftInt = 16;
     clearInterval(intervalId);
     intervalId = setInterval(gameTimer, 1000);
 }
 
+// and clear the interval we set when we want to end countdown
 function endCountdown() {
     clearInterval(intervalId);
 }
-
+//Game reset
 function resetGame() {
-  $("#gametimer").text("Game Over");
-  var secondsLeftInt = 21;
+  endCountdown();
+  $("#gametimer").text("Game Over!");
+// $("#gametimer").css({"color":"red"});
+ 
+// set to 15 (not 16) for display purposes
+//reset key variables
+  var secondsLeftInt = 15;
   var quizQuestionStr = "";
   var answerAStr = "";
   var answerBStr = "";
   var answerCStr = "";
   var answerDStr = "";
   var correctAnswerStr = "";
+// update win loss counts and display 0's
   $("#goodanswer").text(correctCountInt);
   $("#badanswer").text(incorrectCountInt);
 }
-
+// evals player answer
 function evalSubmission(){
+// get letter value for the radio button that was selected and plop into a variable
   var submitValue = $('input[name="selected"]:checked').val();
-  console.log(submitValue)
-  console.log(correctAnswerStr)
+// if the user selects the correct answer
   if  (submitValue === correctAnswerStr)  {
+    // message the user that she chose the correct answer
+   // $("#gametimer").css({"color":"green"});
     $("#gametimer").text("Correct");
+    // increment wins counter and display updated value
     correctCountInt = correctCountInt + 1;
     $("#goodanswer").text(correctCountInt)
+// Increments the counter used to iterate over the quiz array
     quizIndexInt++;
-    console.log("ln 129 correct sub ----quiz index:" + quizIndexInt);
+// Test to see if end of quiz is reached
     if (quizIndexInt === quizMaterialArr.length) {
+// if it is, declare Game Over and reset things
+      $("#gametimer").css({"color":"#000066"});
       $("#gametimer").text("Game Over!");
       $("#goodanswer").text(correctCountInt);
       $("#badanswer").text(incorrectCountInt);
@@ -200,20 +245,22 @@ function evalSubmission(){
       resetGame();
     }
     else {
-      //create separate function to "restart" ????
+//if there are additional questions in array, build another question page
+      $("#gametimer").css({"color":"#000066"});
       removeHTML();
       startCountdown();
       buildQuiz();
-      // startCountdown(); ? if I include this, I get a stack overflow
     }
   }
+// if the player guesses incorrectly
   else {
     $("#gametimer").text("Incorrect");
     incorrectCountInt = incorrectCountInt + 1;
     $("#badanswer").text(incorrectCountInt)
     quizIndexInt++;
-    console.log("ln 142 incorrect sub----quiz index:" + quizIndexInt);
+// checking to see if Quiz array is empty. 
     if (quizIndexInt === quizMaterialArr.length) {
+     // $("#gametimer").css({"color":"red"});
       $("#gametimer").text("Game Over!");
       $("#goodanswer").text(correctCountInt);
       $("#badanswer").text(incorrectCountInt);
@@ -222,7 +269,7 @@ function evalSubmission(){
       resetGame();
     }
     else {
-      //create separate function to "restart" ????
+    // start the quiz anew
       removeHTML();
       startCountdown();
       buildQuiz();
@@ -230,48 +277,42 @@ function evalSubmission(){
     }
   }
 }
-
+// implements a timer
 function gameTimer() {
+  // evals whether timer has hit zero
   if (secondsLeftInt === 0) {
+    // means player let timer expire. Let player know
     $("#gametimer").text("Time's Up!");
     endCountdown();
+    // mark as incorrect and update display
     incorrectCountInt = incorrectCountInt + 1;
     $("#badanswer").text(incorrectCountInt)
     quizIndexInt++;
-    console.log("ln 129 ----quiz index:" + quizIndexInt);
+    // start up a new question - one final time
     removeHTML();
-    gameTimer();
     buildQuiz();
-   
+    startCountdown();
     }
+    // Time remaining!
   else {
+    // decrease timer by 1 second 
     secondsLeftInt --;
     console.log(secondsLeftInt);
     $("#gametimer").text(secondsLeftInt);
     }
 }
-
-function buildQuiz() {  // should I abandon this for loop and keep track of the index[] myself? 
-
+// Pulls data from Quiz Material array and prepares it for display
+function buildQuiz() { 
     quizQuestionStr = quizMaterialArr[quizIndexInt].question;
     answerAStr = quizMaterialArr[quizIndexInt].answers.a;
     answerBStr = quizMaterialArr[quizIndexInt].answers.b;
     answerCStr = quizMaterialArr[quizIndexInt].answers.c;
     answerDStr = quizMaterialArr[quizIndexInt].answers.d;
     correctAnswerStr = quizMaterialArr[quizIndexInt].correctAnswer;
-    /*
-    console.log(quizQuestionStr);
-    console.log(answerAStr);
-    console.log(answerBStr);
-    console.log(answerCStr);
-    console.log(answerDStr);
-    console.log(correctAnswerStr);
-    console.log(positionId);
-   */
     buildHTML();
-    gameTimer();
+    startCountdown();
   }
-
+// Creates and displays quiz data/info dynamically
 function buildHTML() {
   $("#question").html('<p id="question' + positionId + '">' + quizQuestionStr + '</p>');
   $("#answerA").html('<input type="radio" name="selected" id="a1' + positionId + 'answer" value="a">' + 
@@ -282,8 +323,8 @@ function buildHTML() {
   '<label for="q' + positionId + 'answer" value="c">' + '  ' + answerCStr + '</label>');
   $("#answerD").html('<input type="radio" name="selected" id="a4' + positionId + 'answer" value="d">' + 
   '<label for="q' + positionId + 'answer" value="d">' + '  ' + answerDStr + '</label>');
-  // $("submitdiv").html('<button id="submitbtn" type="button" value="Submit" class="button btn">Submit</button>'); 
   }
+  // Function to remove HTML elements of quiz
   function removeHTML() {
     $("#question").html("");
     $("#answerA").html("");
@@ -291,16 +332,5 @@ function buildHTML() {
     $("#answerC").html("");
     $("#answerD").html("");
   }
-/*
-  function hideHTML() {
-  $("#question").hide();
-  $("#answerA").hide();
-  $("#answerB").hide();
-  $("#answerC").hide();
-  $("#answerD").hide();
-  }
-*/
 
-
-// end brcket for document.ready -don't erase
 });
